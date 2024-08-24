f80c84b3bcb6222d8937bb497568b8c8ea843aca
nima1313 <42837650+nima1313@users.noreply.github.com>
2024-08-24 15:36:04 +0330

    Initial commit / JAAM-0.0.1

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,5 +1,5 @@
 abstract class ThingsboardAppConstants {
-  static const thingsBoardApiEndpoint = 'http://localhost:8080';
+  static const thingsBoardApiEndpoint = 'http://iot.darestan.ir:8080';
   static const thingsboardOAuth2CallbackUrlScheme = 'org.thingsboard.app.auth';
 
   /// Not for production (only for debugging)
